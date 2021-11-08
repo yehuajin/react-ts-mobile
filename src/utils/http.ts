93e501309c192dc8ba9259d155ea545cad27d907
yehj <hjye2@iflytek.com>
2021-11-08 09:42:07 +0800

    axios类型优化

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 import { initServerError, initValidate } from './error';
 import envConfig from '@config';
 import { windowLocation } from '@utils/index';
@@ -7,20 +7,25 @@ axios.defaults.baseURL = envConfig.baseURL;
 // 超时时间15s
 axios.defaults.timeout = 15000;
 // Ajax请求
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
+(axios.defaults.headers.common as any)['X-Requested-With'] = 'XMLHttpRequest';
+(axios.defaults.headers.common as any)['Content-Type'] = 'application/json';
 
 // 请求拦截器
 axios.interceptors.request.use(
-  (config) => {
+  (config: AxiosRequestConfig) => {
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error),
 );
 
+interface ResultObj {
+  code?: number;
+  desc?: string;
+  data?: any;
+}
 // 响应拦截器即异常处理
 axios.interceptors.response.use(
-  (response) => {
+  (response: AxiosResponse<ResultObj>) => {
     // 需要根据返回码统一处理公共逻辑
     const result = response.data || {};
     if (result.code !== 0) {
@@ -36,7 +41,7 @@ axios.interceptors.response.use(
   (err) => {
     // 需要根据返回码对错误进行统一处理
     return Promise.reject(initServerError(err));
-  }
+  },
 );
 
 export default axios;
